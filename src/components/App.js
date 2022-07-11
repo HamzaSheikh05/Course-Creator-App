192e583e9c12080a10deef902bf8ef63f4564fbc
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-07-11 22:42:40 +0400

    Display Header at the top of the page

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import { Route } from "react-router-dom";
 import AboutPage from "./about/AboutPage";
+import Header from "./common/Header";
 import HomePage from "./home/HomePage";
 
 function App() {
   return (
     <div className="container-fluid">
+      <Header />
       <Route exact path="/" component={HomePage} />
       <Route path="/about" component={AboutPage} />
     </div>
