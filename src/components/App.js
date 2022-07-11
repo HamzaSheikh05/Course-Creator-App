e4d04b450adc663548d3e23a77f9cf2de55e78de
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-07-11 22:50:19 +0400

    Added 404 page in case of invalid routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,19 @@
 import React from "react";
-import { Route } from "react-router-dom";
+import { Route, Switch } from "react-router-dom";
 import AboutPage from "./about/AboutPage";
 import Header from "./common/Header";
 import HomePage from "./home/HomePage";
+import PageNotFound from "./PageNotFound";
 
 function App() {
   return (
     <div className="container-fluid">
       <Header />
-      <Route exact path="/" component={HomePage} />
-      <Route path="/about" component={AboutPage} />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route path="/about" component={AboutPage} />
+        <Route component={PageNotFound} />
+      </Switch>
     </div>
   );
 }
