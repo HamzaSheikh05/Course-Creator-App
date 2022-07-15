65f084916f3d3233303058de3f18d07cea33f60d
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-07-15 23:03:11 +0400

    Manual mapping of mapDispatchToProps

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -17,7 +17,7 @@ class CoursesPage extends React.Component {
 
   handleSubmit = (event) => {
     event.preventDefault();
-    this.props.dispatch(courseActions.createCourse(this.state.course));
+    this.props.createCourse(this.state.course);
   };
 
   render() {
@@ -41,7 +41,7 @@ class CoursesPage extends React.Component {
 
 CoursesPage.propTypes = {
   courses: PropTypes.array.isRequired,
-  dispatch: PropTypes.func.isRequired,
+  createCourse: PropTypes.func.isRequired,
 };
 
 function mapStateToProps(state, ownProps) {
@@ -50,4 +50,10 @@ function mapStateToProps(state, ownProps) {
   };
 }
 
-export default connect(mapStateToProps)(CoursesPage);
+function mapDispatchToProps(dispatch) {
+  return {
+    createCourse: (course) => dispatch(courseActions.createCourse(course)),
+  };
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(CoursesPage);
