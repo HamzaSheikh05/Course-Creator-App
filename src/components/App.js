09b28ee6622333653319e9be6625b61af53f7d62
HamzaSheikh05 <hrsheikh02@gmail.com>
2022-08-16 20:20:27 +0400

    Configured routing for ManageCoursePage

--- src/components/App.js
+++ src/components/App.js
@@ -5,6 +5,7 @@ import Header from "./common/Header";
 import CoursesPage from "./courses/CoursesPage";
 import HomePage from "./home/HomePage";
 import PageNotFound from "./PageNotFound";
+import ManageCoursePage from "./courses/ManageCoursePage";
 
 function App() {
   return (
@@ -14,6 +15,8 @@ function App() {
         <Route exact path="/" component={HomePage} />
         <Route path="/about" component={AboutPage} />
         <Route path="/courses" component={CoursesPage} />
+        <Route path="/course/:slug" component={ManageCoursePage} />
+        <Route path="/course" component={ManageCoursePage} />
         <Route component={PageNotFound} />
       </Switch>
     </div>
